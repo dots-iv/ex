64ddb516361cee5969d171eefb4a85d7588378c8
dots-iv <dots.iv8@gmail.com>
2023-04-09 16:31:01 +0300

    46456

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
   $('.slider_content').slick({
-    arrows: true,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    speed: 1000,
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    prevArrow: $('.slide_prev'),
+        nextArrow: $('.slide_next'),
   });
+
+  $('.burger').click(function() {
+    $('.header_nav_menu').toggleClass("active")
+    $('.overlay').toggleClass("active")
+    $('.burger').toggleClass("active")
+    $('.body').toggleClass("active")
+    
+  })
 });
 
